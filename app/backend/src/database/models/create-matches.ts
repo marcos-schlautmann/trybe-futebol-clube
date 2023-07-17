2732964995ef813eb508e8e30112d3175ba76a36
Marcos <marcos27.bonfim@gmail.com>
2023-07-17 11:49:39 -0300

    requisito 1 incompleto

---
+++ app/backend/src/database/models/create-matches.ts
@@ -0,0 +1,59 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from "sequelize";
+import db from ".";
+
+class MatchesSequelizeModel extends Model<
+  InferAttributes<MatchesSequelizeModel>,
+  InferCreationAttributes<MatchesSequelizeModel>
+> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchesSequelizeModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    homeTeamId: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    awayTeamId: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "matches",
+    timestamps: false,
+    underscored: true,
+  }
+);
+
+export default MatchesSequelizeModel;
---
+++ app/backend/src/database/models/teams.model.ts
@@ -0,0 +1,37 @@
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  CreationOptional,  
+} from "sequelize";
+import db from ".";
+
+class TeamsSequelizeModel extends Model<
+  InferAttributes<TeamsSequelizeModel>,
+  InferCreationAttributes<TeamsSequelizeModel>
+> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+TeamsSequelizeModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "team_name",
+  },
+}, {
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+    underscored: true,
+});
+
+export default TeamsSequelizeModel;
