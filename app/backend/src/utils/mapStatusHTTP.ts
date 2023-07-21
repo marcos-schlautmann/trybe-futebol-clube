8307e9b35957b7fae5687aade252e8411717c16b
Marcos <marcos27.bonfim@gmail.com>
2023-07-21 18:34:42 -0300

    corrgindo req21

--- app/backend/src/utils/mapStatusHTTP.ts
+++ app/backend/src/utils/mapStatusHTTP.ts
@@ -4,7 +4,7 @@ export default function mapStatusHTTP(status: string): number {
     case 'OK': return 201;
     case 'INVALID_DATA': return 400;
     case 'NOT_FOUND': return 404;
-    case 'CONFLICT': return 409;
+    case 'CONFLICT': return 422;
     case 'UNAUTHORIZED': return 401;
     default: return 500;
   }
